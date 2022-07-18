0ae4c2a56c936c482a2e1604fca45ae5e32b909c
aynp <aryan.pathania2003@gmail.com>
2022-07-18 05:59:34 +0530

    Added option to add product for sellers

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import User from './User.svelte';
   // import { ethers } from 'ethers';
 </script>
+
+<User />
 
 <div>Hello World</div>
---
+++ src/User.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  async function fetchdata() {
+    const response = await fetch('https://dummyjson.com/products');
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) return data;
+    else throw new Error('Something went wrong');
+  }
+</script>
+
+{#await fetchdata()}
+  <p>Awaitinggg</p>
+{:then data}
+  {#each data.products as product}
+    <p>{product.title}</p>
+    <p>{product.description}</p>
+  {/each}
+{:catch error}
+  <p>{error}</p>
+{/await}
+
+<style>
+</style>
