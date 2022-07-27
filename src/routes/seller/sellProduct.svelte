1cb134049bf17143076e6417052dc3fbba843d91
Aryan <aryan.pathania2003@gmail.com>
2022-07-27 22:19:29 +0530

    Refactored

--- src/routes/seller/sellProduct.svelte
+++ src/routes/seller/sellProduct.svelte
@@ -4,20 +4,18 @@
   const productInfo = {
     productID: 0,
     serialNo: 0,
-    tokenID: 0,
   };
 
   const mintNFT = async () => {
-    console.log(productInfo);
     try {
+      const tokenID = productInfo.productID + productInfo.serialNo;
       const tx = await Contract.mintWarrentyNFT(
         wallet.address,
         productInfo.productID,
         productInfo.serialNo,
-        productInfo.tokenID,
+        tokenID
       );
       await tx.wait();
-      console.log(tx);
     } catch (error) {
       console.log('ERROR: ', error);
     }
@@ -28,11 +26,10 @@
   <label for="productID">Product ID</label>
   <input type="text" bind:value={productInfo.productID} />
   <br />
+
   <label for="serialNo">Serial No</label>
   <input type="text" bind:value={productInfo.serialNo} />
   <br />
-  <label for="tokenID">Token ID</label>
-  <input type="text" bind:value={productInfo.tokenID} />
-  <br />
+
   <button type="submit">Submit</button>
 </form>
